37ba46bcc0e21f92185abdde7be6b32cfc0db54a
Arnold Perlas <perlasarnold@gmail.com>
2026-04-15 10:42:12 -0700

    feat: add automated daily security digest workflow and placeholder script

---
+++ tools/security_digest/main.py
@@ -0,0 +1,5 @@
+# Placeholder script
+# TODO: Please copy your real security_digest python code over this file
+
+print("### Daily Security Update")
+print("- Nothing to report today! Please insert your real script into `tools/security_digest/main.py`")
